0f6a3787d91f6db3cacef48e2d7733bd0357da83
SamandarTurdiyev <124241474+SamandarTurdiyev@users.noreply.github.com>
2023-02-27 17:21:23 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,254 @@
+// const isDriverLicence = true;
+// const isVisibly = true;
+
+// if(isDriverLicence && isVisibly ){
+//     console.log(' siz mashina hayday olasiz ')
+// } else {
+//     console.log(' siz mashina hayday olmaysiz ')
+// }
+
+
+
+
+// const johnWeight = +prompt('Jonning vazni');
+// const johnHeight = +prompt('Jonning boyi');
+// const bobWeight = +prompt('Bobning vazni');
+// const bobHeight = +prompt('Bobning boyi');
+
+// const johnBmi = Math.round(johnWeight / johnHeight ** 2);
+// const bobBmi = Math.round(bobWeight / bobHeight ** 2);
+
+// console.log(johnBmi);
+// console.log(bobBmi);
+
+// if (johnBmi > bobBmi) {
+//     console.log('John BMI katta');
+// } else if (johnBmi === bobBmi) {
+//     console.log('Ikkisi ham teng');
+// } else {
+//     console.log('Bob BMI katta');
+// }
+
+
+
+
+
+
+// const num1 = +prompt ('son kiriting')
+//  if (num1 > 0){
+//     alert(num1 + 1)
+//  }
+
+
+
+//  const num2 = +prompt ('son kiriting')
+//  if (num2 > 0){
+//     alert(num2 + 1)
+//  } else {
+//     alert(`shu son ${num2 -2}`)
+//  }
+
+
+
+// const num3 = +prompt ('xoxlagan soningizd liriting')
+// if (num3 > 0){
+//     alert(num3 + 1)
+// } else if (num3 == 0 ){
+//     alert(10)
+// } else {
+//     alert(num3 + 2)
+// }
+
+
+
+
+
+// counter = 0;
+// let num4 = +prompt('birinchi sonni kiriting')
+// if (num4 > 0) {
+//     counter++
+// }
+// let num5 = +prompt('ikkinchi sonni kiriting')
+// if (num5 > 0){
+//     counter++
+// }
+// let num6 = +prompt('uchinchi sonni kiriting')
+// if (num6 > 0){
+//     counter++
+// }
+
+// alert(counter)
+
+
+
+
+
+
+
+// positive= 0;
+// minus = 0;
+
+// let num7 = +prompt('birinchi sonni kiriting')
+// if (num7 > 0) {
+//     positive++
+// } else {
+//     minus++
+// }
+// let num8 = +prompt('ikkinchi sonni kiriting')
+// if (num8 > 0){
+//     positive++
+// } else {
+//    minus++
+// }
+// let num9 = +prompt('uchinchi sonni kiriting')
+// if (num9 > 0){
+//     positive++
+// } else {
+//     minus++
+// }
+
+// alert('manfiy sonlar ' + minus + 'ta' + ' va ' +' musbat sonlar ' + positive +  'ta')
+
+
+
+
+// const num10  = +prompt ('iltimos birinchi son kiriting')
+// const num11 = +prompt('iltimos ikkinchi sonni ham kiriting')
+
+// if (num10 > num11){
+//     console.log(`birinchi son katta ekan ${num10} > ${num11} dan`)
+// } else if(num10 == num11) {
+//     console.log (`ikkala son ham teng ekan ${num10} va ${num11}`)
+// } else {
+//     console.log (`ikkinchi son katta ekan ${num11} > ${num10} dan`)
+// }
+
+
+
+
+
+// const num12  = +prompt ('iltimos birinchi son kiriting')
+// const num13 = +prompt('iltimos ikkinchi sonni ham kiriting')
+
+// if (num12 > num13){
+//     console.log(`birinchi son kichik ekan ${num13} < ${num12} dan`)
+// } else if(num12 == num13) {
+//     console.log (`ikkala son ham teng ekan ${num12} va ${num13}`)
+// } else {
+//     console.log (`ikkinchi son kichik ekan ${num12} < ${num13} dan`)
+// }
+
+
+
+
+// const num14 = +prompt('iltimos birinchi sonni kiriting')
+// const num15 = +prompt('iltimos ikkinchi sonni kiriting')
+
+// if (num14 < num15){
+//     console.log(`sizning javobingiz tartib raqami ${num14} `)
+// } else if(num14 == num15){
+//     console.log(`sizning javobingiz tartib raqami ikkalasi ham teng ${num14} va ${num15} `)
+// } else {
+//     console.log(`sizning javobingiz tartib raqami ${num15}`)
+// }
+
+
+
+// const num16 = +prompt('iltimos birinchi sonni kiriting')
+// const num17 = +prompt('iltimos ikkinchi sonni kiriting')
+
+// if(num16 > num17){
+//     console.log(`sizning kiritgan katta raqamiz ${num16} va kichik raqamiz ${num17} `)
+// } else { 
+//     console.log(`sizning kiritgan katta raqamiz ${num17} va kichik raqamiz ${num16} `)
+// }
+
+
+const a = +prompt('iltimos birinchi son kiriting')
+const b = +prompt('iltimos ikkinchi sonni ham kiriting')
+const info = (a * 0 + a);
+const info2 = (b * 2 + a);
+if (info < info2) {
+    alert(`birinchi son ${info}<${info2} dan`);
+} else {
+    alert("eror");
+}
+
+
+
+
+
+// const johnWeight = +prompt('Jonning vazni');
+// const johnHeight = +prompt('Jonning boyi');
+// const bobWeight = +prompt('Bobning vazni');
+// const bobHeight = +prompt('Bobning boyi');
+
+// const johnBmi = Math.round(johnWeight / johnHeight ** 2);
+// const bobBmi = Math.round(bobWeight / bobHeight ** 2);
+
+
+// if (johnBmi > bobBmi) {
+//     console.log(`Johnni BMI  ${johnBmi} kattaroq BoBni BMI ${bobBmi}dan ` );
+// } else if (johnBmi === bobBmi) {
+//     console.log('Ikkisi ham teng');
+// } else {
+//     console.log(`Bobni BMI  ${bobBmi} kattaroq Johnni BMI ${johnBmi}dan `);
+// }
+
+
+
+
+// const userAge = +prompt('sizning yoshingiz nechida')
+
+// if (userAge > 0  && userAge < 18){
+//     console.log(`siz hali oqishingiz kerak`)
+// } else if (userAge > 18 && userAge < 60){
+//     console.log(`siz faqat mehnat qilishingiz kerak`)
+// } else{
+//     console.log(` siz nafaqaga chiqgansiz va bunga ${userAge - 60} yil bolgan nafaqangizdan nevalaringizga ham berib turing `)
+// }
+
+
+
+
+// const num20 = +prompt('iltimos birinchi sonni kiriting')
+// const num21 = +prompt('iltimos ikkinchi sonni kiriting')
+// const num22 = +prompt('iltimos uchinchi sonni kiriting')
+
+// if (num20 < num21 && num20 < num22){
+//     console.log(`kiritgan sonlariz orasida kichigi ${num20}`)
+// } else if (num21 < num20 && num21 < num22){
+//     console.log(`kiritgan sonlariz orasida ichigi ${num21}`)
+// } else{
+//     console.log(`kiritgan sonlariz orasida kichigi ${num22}`)
+// }
+
+
+
+// const num23 = +prompt('iltimos birinchi sonni kiriting')
+// const num24 = +prompt('iltimos ikkinchi sonni kiriting')
+// const num25 = +prompt('iltimos uchinchi sonni kiriting')
+
+// if ( num23 > num24 && num23 < num25){
+//     console.log(`kiritgan sonlariz orasida o'rtachasi  ${num23}`)
+// } else if (num24 > num23  && num24 < num25){
+//     console.log(`kiritgan sonlariz orasida o'rtanchasi ${num24}`)
+// } else{
+//     console.log(`kiritgan sonlariz orasida o'rtanchasi ${num25}`)
+// }
+
+
+
+
+
+// const num26 = +prompt('iltimos birinchi sonni kiriting')
+// const num27= +prompt('iltimos ikkinchi sonni kiriting')
+// const num28 = +prompt('iltimos uchinchi sonni kiriting')
+
+// if(num26 < num27 && num28 > num26 && num28 > num27 && num26 < num28 ){
+//     console.log(`sizning kiritgan kichik raqamiz ${num26} va katta raqamiz ${num28} `)
+// } else if( num27 < num26 && num26 > num28 && num26 > num27 && num27 < num28){
+//     console.log(`sizning kiritgan kichik raqamiz ${num28} va katta raqamiz ${num27}`)
+// } else{
+//     console.log(`sizning kiritgan kichik raqamiz ${num27} va katta raqamiz ${num28}`)
+// } 
